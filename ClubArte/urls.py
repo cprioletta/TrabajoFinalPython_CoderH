9fbd1f527412b1d2844b19c8eabcadbebf202ca5
Chiara P <cprioletta@udesa.edu.ar>
2023-04-06 16:38:44 -0300

    Creación de el apartado para cinematografía

--- ClubArte/urls.py
+++ ClubArte/urls.py
@@ -3,6 +3,7 @@ from ClubArte.views import *
 
 urlpatterns = [
     path("", Main, name="ClubArteMain"),
+
     path("Poesia/Crear/",poesia_crear, name="CrearPoesia"),
     path("Poesia/Eliminar/<int:id_poesia>", poesia_elim, name="EliminarPoesia"),
     path("Poesia/Modificar/<int:id_poesia>", poesia_modif, name="ModificarPoesia"),
@@ -10,4 +11,10 @@ urlpatterns = [
     path('Poesia/Buscar/', poesia_buscar, name='PoesiaBuscar'),
     path('Poesia/BuscarVer/', poesia_buscar_ver, name='PoesiaBuscarVer'),
 
+    path("Cine/Crear/", cine_crear, name="CrearCine"),
+    path("Cine/Eliminar/<int:id_movie>", cine_elim, name="EliminarCine"),
+    path("Cine/Modificar/<int:id_movie>", cine_modif, name="ModificarCine"),
+    path('Cine/Ver/<int:id_movie>', cine_ver, name='VerCine'),
+    path('Cine/Buscar/', cine_buscar, name='BuscarCine'),
+    path('Cine/BuscarVer/', cine_buscar_ver, name='BuscarVerCine'),
 ]
